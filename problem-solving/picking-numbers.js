2d310603ef509bcf6a97f79a0d8c066c1c7fbd76
Muhammad Farrel <farrelputrapambudi.2020@gmail.com>
2025-09-22 08:46:47 +0700

    add solution picking numbers

---
+++ problem-solving/picking-numbers.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'pickingNumbers' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+
+function pickingNumbers(a) {
+    // Write your code here
+    let frequency = new Array(101).fill(0);
+    let maxPair = 1;
+
+    for(const item of a) {
+        frequency[item]++;
+    };
+
+    for(let i = 1; i < frequency.length; i++) {
+        const currentPair = frequency[i] + frequency[i+1];
+
+        if(currentPair > maxPair) {
+            maxPair = currentPair;
+        };
+    };
+
+    return maxPair;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const a = readLine().replace(/\s+$/g, '').split(' ').map(aTemp => parseInt(aTemp, 10));
+
+    const result = pickingNumbers(a);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
