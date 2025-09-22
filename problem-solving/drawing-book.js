608395227a857b21bdae32c1cfae20662554159f
Muhammad Farrel <farrelputrapambudi.2020@gmail.com>
2025-09-22 07:16:38 +0700

    add solution drawing book

---
+++ problem-solving/drawing-book.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'pageCount' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER p
+ */
+
+function pageCount(n, p) {
+    // Write your code here
+    let total = Math.trunc(n/2);
+    let depan = Math.trunc(p/2);
+    let belakang = total - depan;
+
+    console.log(total, depan, belakang);
+
+    if(depan < belakang) {
+        return depan;
+    } else {
+        return belakang;
+    };
+};
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const p = parseInt(readLine().trim(), 10);
+
+    const result = pageCount(n, p);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
