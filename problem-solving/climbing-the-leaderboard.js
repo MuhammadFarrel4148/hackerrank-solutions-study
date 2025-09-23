08821979ab1a52350836abdf4f9635c769a9786d
Muhammad Farrel <farrelputrapambudi.2020@gmail.com>
2025-09-23 20:46:35 +0700

    add solution climbing the leaderboard

---
+++ problem-solving/climbing-the-leaderboard.js
@@ -0,0 +1,68 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'climbingLeaderboard' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY ranked
+ *  2. INTEGER_ARRAY player
+ */
+
+function climbingLeaderboard(ranked, player) {
+    // Write your code here
+    const uniqueRanked = [...new Set(ranked)];
+    let index = uniqueRanked.length - 1;
+    const result = [];
+
+    for(const aliceScore of player) {
+        while(index >= 0 && aliceScore >= uniqueRanked[index]) {
+            index--
+        };
+
+        const aliceRank = index + 2;
+        result.push(aliceRank);
+    };
+
+    return result;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const rankedCount = parseInt(readLine().trim(), 10);
+
+    const ranked = readLine().replace(/\s+$/g, '').split(' ').map(rankedTemp => parseInt(rankedTemp, 10));
+
+    const playerCount = parseInt(readLine().trim(), 10);
+
+    const player = readLine().replace(/\s+$/g, '').split(' ').map(playerTemp => parseInt(playerTemp, 10));
+
+    const result = climbingLeaderboard(ranked, player);
+
+    ws.write(result.join('\n') + '\n');
+
+    ws.end();
+}
