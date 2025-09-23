ea1d8fd2746f9d68cce71292f61a66fd7cb7de42
Muhammad Farrel <farrelputrapambudi.2020@gmail.com>
2025-09-23 21:39:43 +0700

    add solution beautiful days at the movie

---
+++ problem-solving/beautiful-days-at-the-movie.js
@@ -0,0 +1,69 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'beautifulDays' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER i
+ *  2. INTEGER j
+ *  3. INTEGER k
+ */
+
+function beautifulDays(i, j, k) {
+    // Write your code here
+    let count = 0;
+
+    for(let iLoop = i; iLoop <= j; iLoop++) {
+        const reversedString = iLoop.toString().split('').reverse().join('');
+        const reversedNumber = parseInt(reversedString);
+
+        const result = Math.abs(iLoop - reversedNumber);
+
+        if(result % k === 0) {
+            count++;
+        };
+    };
+    
+    return count;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const i = parseInt(firstMultipleInput[0], 10);
+
+    const j = parseInt(firstMultipleInput[1], 10);
+
+    const k = parseInt(firstMultipleInput[2], 10);
+
+    const result = beautifulDays(i, j, k);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
