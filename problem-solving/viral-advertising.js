180938feab80c38f348b77e475f365c03e1087a3
Muhammad Farrel <farrelputrapambudi.2020@gmail.com>
2025-09-24 09:20:23 +0700

    add solution viral advertising

---
+++ problem-solving/viral-advertising.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'viralAdvertising' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER n as parameter.
+ */
+
+function viralAdvertising(n) {
+    // Write your code here
+    let shared = 5;
+    let liked = 2;
+    let cumulative = 2;
+
+    for(let i = 2; i <= n; i++) {
+        shared = Math.floor(shared/2) * 3;
+        liked = Math.floor(shared/2);
+        cumulative = cumulative + liked;
+    };
+
+    return cumulative;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const result = viralAdvertising(n);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
